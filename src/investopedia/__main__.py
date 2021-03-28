319874e22b1d6fdce2e4b84f12a2b1930843ad15
dmontaner <david.montaner@gmail.com>
2021-03-29 00:05:46 +0100

    added pdf flag to main

--- src/investopedia/__main__.py
+++ src/investopedia/__main__.py
@@ -5,6 +5,7 @@ import investopedia as iv
 def main():
     parser = argparse.ArgumentParser(description='Investopedia page downloader:')
     parser.add_argument('-v', '--version', action='version', version=iv.__version__)
+    parser.add_argument('-p', '--no-pdf', help='Do not attempt to create PDF file', action='store_true')
     parser.add_argument('-o', '--output-file', type=str, help='output file')
     parser.add_argument('-m', '--input-file' , type=str, help='input file')
     parser.add_argument('urls', help='urls you want to download.', nargs="+")
@@ -12,16 +13,18 @@ def main():
 
     # parse args
     urls = args.urls
+    pdf = not args.no_pdf
 
     if args.output_file:
         output_file = args.output_file
     else:
-        output_file = 'salida_investopedia.html'
+        output_file = 'generated_investopedia_book.html'
 
     # pipeline
     pages = iv.download_pages(urls)
     filename = iv.save_html(pages, output_file)
-    iv.convert_pdf(filename)
+    if pdf:
+        iv.convert_pdf(filename)
 
 
 if __name__ == "__main__":
